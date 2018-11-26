9618f429b62e91096a606603f894ff7546b82120
Erd <erd@conceptual.ca>
2018-11-26 14:39:56 -0500

    ...

--- download.lua
+++ download.lua
@@ -8,17 +8,18 @@ fs.makeDirectory("/usr/lib/oc-pkgr-tmp/");
 shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-lib/master/lib/json.lua" "/usr/lib/json.lua"');
 package.loaded["json"] = nil;
 
---shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/install/lib/oc-pkgr/OCPkgrPackageParser.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrPackageParser.lua"');
-shell.execute('cp "./install/lib/oc-pkgr/OCPkgrPackageParser.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrPackageParser.lua"');
+shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/install/lib/oc-pkgr/OCPkgrPackageParser.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrPackageParser.lua"');
+--shell.execute('cp "./install/lib/oc-pkgr/OCPkgrPackageParser.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrPackageParser.lua"');
 
 
---shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/install/lib/oc-pkgr/OCPkgrFileDownloadUtil.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrFileDownloadUtil.lua"');
-shell.execute('cp "./install/lib/oc-pkgr/OCPkgrFileDownloadUtil.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrFileDownloadUtil.lua"');
+shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/install/lib/oc-pkgr/OCPkgrFileDownloadUtil.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrFileDownloadUtil.lua"');
+--shell.execute('cp "./install/lib/oc-pkgr/OCPkgrFileDownloadUtil.lua" "/usr/lib/oc-pkgr-tmp/OCPkgrFileDownloadUtil.lua"');
 
---shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/oc-pkgr.json" "/tmp/oc-pkgr.json"');
-shell.execute('cp "./oc-pkgr.json" "/tmp/oc-pkgr.json"');
+shell.execute('wget -f "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/oc-pkgr.json" "/tmp/oc-pkgr.json"');
+--shell.execute('cp "./oc-pkgr.json" "/tmp/oc-pkgr.json"');
 
-package.loaded["oc-pkgr/OCPkgrPackageParser"] = nil;
+
+package.loaded["oc-pkgr-tmp/OCPkgrPackageParser"] = nil;
 local parser = require("oc-pkgr-tmp/OCPkgrPackageParser");
 local packageData = parser.parse("/tmp/oc-pkgr.json");
 
@@ -29,98 +30,11 @@ dl.downloadInstallDependancies(packageData);
 require("import");
 import.clearCache();
 
-shell.execute('cp "./install/lib/oc-pkgr/OCPkgr.lua" "/usr/lib/oc-pkgr-tmp/OCPkgr.lua"');
-
+--shell.execute('cp "../oc-git/lib/git.lua" "/usr/lib/git.lua"');
+--shell.execute('cp "./install/lib/oc-pkgr/OCPkgr.lua" "/usr/lib/oc-pkgr-tmp/OCPkgr.lua"');
+package.loaded["oc-pkgr-tmp/OCPkgr"] = nil;
 local OCPkgr = import("oc-pkgr-tmp/OCPkgr");
 
 local pkgMan = OCPkgr:new();
 
-
-
 pkgMan:installPackageManager(packageData, "/usr");
-
---[[
-
-    local os = require("os");
-    local pkgrJson;
-    
-    local jsonLibLoaded, jsonLib = pcall(require, "json");
-    
-    print("Installing oc-pkgr");
-    
-    if not jsonLibLoaded then
-        print("Downloading JSON Lib...");
-        fs.makeDirectory("/tmp/oc-pkgr/");
-        shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-lib/master/lib/json.lua" "/tmp/lib/json.lua"');
-        package.loaded["json"] = nil;
-        jsonLib = require("json");
-        print("done.");
-    end
-    
-    local cwd = shell.resolve("./");
-    
-    print('cwd: ' .. cwd);
-    
-    local localPkgrPath = fs.concat(cwd, "oc-pkgr.json");
-    
-    if fs.exists(localPkgrPath) then
-        print("Found local oc-pkgr.json");
-        pkgrJson = localPkgrPath;
-    else 
-        print("Downloading oc-pkgr.json...");
-        fs.makeDirectory("/tmp/oc-pkgr/");
-        shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-pkgr/master/oc-pkgr.json" "/tmp/oc-pkgr/oc-pkgr.json"');
-        pkgrJson = "/tmp/oc-pkgr/oc-pkgr.json";
-        print("done.");
-    end 
-    
-    local pkg = "";
-    local pkgHandle = fs.open(pkgrJson, "r");
-    local chunk = pkgHandle:read(100);
-    while chunk  do
-        pkg = pkg .. chunk;
-        chunk = pkgHandle:read(100);
-    end
-    pkgHandle:close();
-    
-    
-    local pkjObj = jsonLib.decode(pkg);
-    
-    local installFiles = pkjObj.installFiles;
-    --[[
-        
-        local env = _G;
-        print(env);
-        for k,v in pairs(env) do
-            print(k);
-        end
-        ]]
-        --[[
-
-            
-            fs.remove("/tmp/oc-pkgr/install");
-            
-            for k,v in pairs(installFiles) do
-                local localPath = fs.concat(cwd, "installerFiles", k);
-                local dir = fs.path(k);
-                local name = fs.name(k);
-                local tmpDir = fs.concat("/tmp/oc-pkgr/install", dir);
-                local tmpPath = fs.concat(tmpDir, name);
-                
-                
-                fs.makeDirectory(tmpDir);
-                
-                if fs.exists(localPath) then
-                    print('Copy ' .. localPath .. ' to ' .. tmpPath);
-                    fs.remove(tmpPath);
-                    fs.copy(localPath, tmpPath);
-                else 
-                    print("Downloading " .. k .. "...");
-                    shell.execute('wget -fq "' .. v .. '" "' .. tmpPath ..'"');
-                    print("done.");
-                end
-            end
-            
-            shell.execute('ls -al /tmp/oc-pkgr/install');
-            shell.execute('cd /tmp/oc-pkgr/install && ./install');
-            ]]
--- install/lib/oc-pkgr/OCPkgr.lua
+++ install/lib/oc-pkgr/OCPkgr.lua
@@ -6,11 +6,13 @@ local shell = require("shell");
 local fs = require("filesystem");
 local Class = import("oop/Class");
 local semver = import("semver");
+local json = import("json");
+
+local transfer = import("tools/transfer")
 
 local OCPkgr = Class("OCPkgr");
 function OCPkgr:initialize()
-    self.packagesDiscovered = {};
-
+    
 end
 
 function OCPkgr:_pkg(packageDataOrPath)
@@ -22,54 +24,119 @@ function OCPkgr:_pkg(packageDataOrPath)
 end
 
 function OCPkgr:installPackageManager(packageDataOrPath, dest);
+    self.packagesDiscovered = {};
+    self.filesDiscovered = {};
+
     local pkg = self:_pkg(packageDataOrPath);
     print("installing oc-pkgr...");
+
+    if pkg.fileDependancies then
+        for k, v in pairs(pkg.fileDependancies) do
+            self.filesDiscovered[k] = v;
+        end
+    end
+
     if pkg.packageDependancies then
         for k, v in pairs(pkg.packageDependancies) do
-            self:installPackage(k, v, dest);
+            self:discoverPackages(k, v);
         end
     end
+    self:_doInstall(dest);
 end
 
+function OCPkgr:_doInstall(dest)
 
-function OCPkgr:installPackage(githubPath, version, dest)
-    version = version or "";
-    dest = dest or 'usr';
-
-    local repo = GithubRepo:new(githubPath);
-    repo:findVersion(version);
---[[
-
-    local pkgPath = "https://raw.githubusercontent.com/" .. githubPath .."/master/oc-pkgr.json";
-    if version ~= "" then
-        pkgPath = "https://raw.githubusercontent.com/" .. githubPath .."/" .. version .. "/oc-pkgr.json";
+    for d, s in pairs(self.filesDiscovered) do
+        shell.execute('wget -f "' .. s .. '" "' .. d .. '"');
     end
     
-    
-    local fname = githubPath:gsub("/", "-")
-    local targetPath = '/tmp/' .. fname ..  '-oc-pkgr.tmp.json';
-    shell.execute('wget -f "' .. pkgPath .. '" "' .. targetPath .. '"');
-    
-    
-    if not fs.exists(targetPath) then 
-        print("could not get package file for " .. githubPath .. " #" .. (version or 'master'));
-    else 
-        local parsed, result = pcall(OCPkgrPackageParser.parse, targetPath);
-        if not parsed then
-            print("could not get package file for " .. githubPath .. " #" .. (version or 'master'));
-        else
-            print(result.name);
-            
-            
+    for pkgName, info  in pairs(self.packagesDiscovered) do
+        local tmpPath = "/usr/oc-pkgr/tmp/packages/" .. pkgName .. '/' .. info.sha;
+        fs.makeDirectory(tmpPath);
+        local repo = GithubRepo:new(pkgName);
+        repo:checkout(tmpPath, info.sha);
+        local pkg = self:_pkg(tmpPath .. '/oc-pkgr.json');
+        print("Installing " .. pkgName .. "...");
+        if pkg.install then
+            for d,s in pairs(pkg.install) do
+                
+                local copyFrom = tmpPath .. '/' .. s;
+
+                local copyTo;
+                if d:sub(1,1) == '/' then 
+                    copyTo = d;
+                else 
+                    copyTo = dest .. '/' .. d;
+                end
+
+                local toPath = fs.path(copyTo);
+                local toName = fs.name(copyTo);
+                if copyTo:sub(string.len(copyTo)) == '/' then
+                    toPath = toPath .. '/' .. toName;
+                end 
+
+                fs.makeDirectory(toPath);
+
+                local cpCmd = 'cp -r -f ' .. copyFrom ..' ' .. copyTo;
+                print(cpCmd);
+                shell.execute(cpCmd);
+            end
+        end
+        print("");
+    end
+
+end
+
+function OCPkgr:discoverPackages(githubPath, version)
+    local repo = GithubRepo:new(githubPath);
+    local versionInfo = repo:findVersion(version);
+    if not versionInfo then
+        error("Could not find " .. githubPath .. " " .. version);
+    end
+
+    local currentVersionInfo = self.packagesDiscovered[githubPath];
+    local newFound = false;
+    if currentVersionInfo == nil then
+        self.packagesDiscovered[githubPath] = versionInfo;
+        newFound = true;
+    else
+        local curFirst = tostring(currentVersionInfo.version):sub(1,1);
+        local newFirst = tostring(versionInfo.version):sub(1,1);
+
+        if tostring(versionInfo.version) == '@latest' then
+            self.packagesDiscovered[githubPath] = versionInfo;
+            newFound = true;
+        elseif curFirst ~= '#' and newFirst == '#' then
+            self.packagesDiscovered[githubPath] = versionInfo;
+            newFound = true;
+        elseif  curFirst ~= '#' and newFirst ~= '#' then
+            if newFirst > curFirst then
+                self.packagesDiscovered[githubPath] = versionInfo;
+                newFound = true;
+            end
+        end
+    end
+    if newFound then
+        local info = self.packagesDiscovered[githubPath];
+        local s, pkgFile = pcall(repo.getFile, repo, info.sha, "oc-pkgr.json");
+
+        if s then
+            local pkg = json.decode(pkgFile);
+            local pkgDeps = pkg.packageDependancies;
+            if pkgDeps then
+                for k, v in pairs(pkgDeps) do
+                    self:discoverPackages(k, v);
+                end
+            end
+            local fileDeps = pkg.fileDependancies;
+            if fileDeps then
+                for k, v in pairs(fileDeps) do
+                    self.filesDiscovered[k] = v;
+                end
+            end
         end
         
     end
-    
-    
-    if not self.packagesLoaded[githubPath] then
-        
-    end
-    ]]
 end
 
 return OCPkgr;
